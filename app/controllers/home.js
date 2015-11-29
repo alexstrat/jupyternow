a9990dc7e1ff0f476633499943c3e279dc5573ef
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2015-11-28 19:23:02 -0800

    Add /dispatch route
    According to user being logged in, redirect to his sever. Create the
    server if not server.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   router = express.Router(),
-  Article = require('../models/article');
+  Server = require('../models/server');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -12,3 +12,31 @@ router.get('/', function (req, res, next) {
       user: req.user
     });
 });
+
+router.get('/dispatch', function (req, res, next) {
+    var user = req.user;
+
+    if(!req.user) {
+        return res.send(401);
+    }
+
+    Server.findByUserId(user.id).then(function(servers){
+        switch(servers.length) {
+            case 1:
+                var server = servers[0];
+                res.redirect('/s/'+server.slug);
+                break;
+            case 0:
+                Server.createAndStartDefaultServerForUser(user)
+                    .then(function(server) {
+                        res.redirect('/s/'+server.slug);
+                    });
+                    break;
+            default:
+                // FIX ME: we want to display a UI to chose server to use
+                var server = servers[0];
+                res.redirect('/s/'+server.slug);
+        }
+    });
+});
+
